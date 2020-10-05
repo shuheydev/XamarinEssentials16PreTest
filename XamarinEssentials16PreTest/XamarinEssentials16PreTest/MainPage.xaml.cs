fd9dab8b2dc69f551c4ee67226ccaad32a837463
shuheydev <shuhey.dev@gmail.com>
2020-10-05 21:45:42 +0900

    Add project files.

---
+++ XamarinEssentials16PreTest/XamarinEssentials16PreTest/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace XamarinEssentials16PreTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var permissionContactRead = await Permissions.CheckStatusAsync<Permissions.ContactsRead>();
+            var permissionContactWrite = await Permissions.CheckStatusAsync<Permissions.ContactsWrite>();
+            var selected =await Xamarin.Essentials.Contacts.PickContactAsync();
+
+            if (selected is null)
+                return;
+        }
+    }
+}
